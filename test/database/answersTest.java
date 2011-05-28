79edd6e6f257c9dc9dd530166725274b498097e7
David Fairbrother <d.fairbrother@uq.edu.au>
2011-05-28 13:34:19 +1000

    * Added testgetAnswer()

--- test/database/answersTest.java
+++ test/database/answersTest.java
@@ -20,6 +20,7 @@ public class answersTest {
     Connection conn;
     
     public answersTest() {
+
     }
 
     @BeforeClass
@@ -36,6 +37,8 @@ public class answersTest {
     
     @After
     public void tearDown() {
+
+        
     }
 
     /**
@@ -44,12 +47,25 @@ public class answersTest {
     @Test
     public void testAddAnswer() {
         System.out.println("addAnswer");
-        answers instance = new answers();
-        int expResult = 0;
-        int result = instance.addAnswer();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        answers instance = new answers(10, 5, "4", "answertext", "N");
+        instance.addAnswer();
+                
+        try {
+            reset();
+            getOracleConnection();
+            PreparedStatement statement = conn.prepareStatement("SELECT * FROM Answers WHERE answerID='10'");
+            ResultSet rset = statement.executeQuery();
+            while(rset.next()) {
+                assertTrue(rset.getNString("answer").matches("answertext"));
+            }
+            rset.close();
+            statement.close();
+            conn.close();
+        }
+        catch(SQLException e) {
+            System.out.println("ERROR OCCURRED IN TEST: " + e.toString());
+        }
     }
 
     /**
@@ -58,12 +74,52 @@ public class answersTest {
     @Test
     public void testEditAnswer() {
         System.out.println("editAnswer");
-        answers instance = new answers();
-        int expResult = 0;
-        int result = instance.editAnswer();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        getOracleConnection();
+        
+        try {
+            try {
+                PreparedStatement statement = conn.prepareStatement("DELETE FROM Answers");
+                statement.execute();
+                statement.close();
+                conn.close();
+            }
+            catch(SQLException e) {
+                System.out.println("Error in tearDown() SQL: " + e.toString());
+            }
+            getOracleConnection();
+            PreparedStatement statement = conn.prepareStatement("INSERT INTO Answers(answerID, keypad, answer, "
+                        + "questID, correct) VALUES (?, ?, ?, ?, ?)");
+            statement.setInt(1, 11);
+            statement.setString(2, "2");
+            statement.setString(3, "answer");
+            statement.setInt(4, 4);
+            statement.setString(5, "N");
+            statement.executeUpdate();
+            statement.close();
+            conn.close();
+        }
+        catch(SQLException e) {
+            System.out.println("Error occurs in test edit answer: " + e.toString());
+        }
+        
+        answers instance = new answers(11, 4, "2", "answer1", "Y");
+        instance.editAnswer();
+        
+        
+        getOracleConnection();
+        try {
+            PreparedStatement statement = conn.prepareStatement("SELECT * FROM Answers WHERE answerID='11'");
+            ResultSet rset = statement.executeQuery();
+            while(rset.next()) {
+                assertTrue(rset.getNString("answer").matches("answer1"));
+            }
+            rset.close();
+            statement.close();
+            conn.close();
+        }
+        catch(SQLException e) {
+            System.out.println("Error occurs in test edit answer 2: " + e.toString());
+        }
     }
 
     /**
@@ -72,12 +128,43 @@ public class answersTest {
     @Test
     public void testDeleteAnswer() {
         System.out.println("deleteAnswer");
-        answers instance = new answers();
-        int expResult = 0;
-        int result = instance.deleteAnswer();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        getOracleConnection();
+        
+        try {
+            PreparedStatement statement = conn.prepareStatement("INSERT INTO Answers(answerID, keypad, answer, "
+                        + "questID, correct) VALUES (?, ?, ?, ?, ?)");
+            statement.setInt(1, 12);
+            statement.setString(2, "2");
+            statement.setString(3, "answer");
+            statement.setInt(4, 4);
+            statement.setString(5, "N");
+            statement.execute();
+            statement.close();
+            conn.close();
+        }
+        catch(SQLException e) {
+            System.out.println("Error occurs in testDeleteAnswer(): " + e.toString());
+        }
+        
+        answers instance = new answers(12, 4, "2", "answer1", "Y");
+        instance.deleteAnswer();
+        
+        getOracleConnection();
+        
+        try {
+            PreparedStatement statement = conn.prepareStatement("SELECT * FROM Answers WHERE answerID='12'");
+            ResultSet rset = statement.executeQuery();
+            while(rset.next()) {
+                assertTrue(false);
+            }
+            assertTrue(true);
+            rset.close();
+            statement.close();
+            conn.close();
+        }
+        catch(SQLException e) {
+            System.out.println("Error occurs in testDeleteAnswer() 2: " + e.toString());
+        }
     }
 
     /**
@@ -86,12 +173,27 @@ public class answersTest {
     @Test
     public void testGetAnswer() {
         System.out.println("getAnswer");
-        answers instance = new answers();
-        int expResult = 0;
-        int result = instance.getAnswer();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        answers instance = new answers(12, 4, "2", "answer1", "N");
+        getOracleConnection();
+        
+        try {
+            PreparedStatement statement = conn.prepareStatement("INSERT INTO Answers(answerID, keypad, answer, "
+                        + "questID, correct) VALUES (?, ?, ?, ?, ?)");
+            statement.setInt(1, 12);
+            statement.setString(2, "2");
+            statement.setString(3, "answer");
+            statement.setInt(4, 4);
+            statement.setString(5, "N");
+            statement.execute();
+            statement.close();
+            conn.close();
+        }
+        catch(SQLException e) {
+            System.out.println("Error occurs in testGetAnswer(): " + e.toString());
+        }
+        instance.getAnswer();
+        assertEquals(4, instance.getQuestID());
+
     }
 
     /**
@@ -193,13 +295,30 @@ public class answersTest {
      */
     @Test
     public void testSetCorrect() {
+        
         System.out.println("setCorrect");
         answers instance = new answers(10, 5, "test", "answertext", "N");
         instance.setCorrect("Y");
         assertTrue(instance.getCorrect().matches("Y"));
+        
     }
     
-        /**
+    @Test
+    public void reset() {
+        getOracleConnection();
+        try {
+            PreparedStatement statement = conn.prepareStatement("DELETE FROM Answers");
+            statement.execute();
+            statement.close();
+            conn.close();
+        }
+        catch(SQLException e) {
+            System.out.println("Error in tearDown() SQL: " + e.toString());
+        }
+        assertTrue(true);
+    }
+    
+     /**
      * Establishes a Oracle connection.
      */
     private Connection getOracleConnection() {
@@ -208,10 +327,11 @@ public class answersTest {
             /* Load the Oracle JDBC Driver and register it. */
             DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
             /* Open a new connection */
-            conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "username", "password");
+            conn = DriverManager.getConnection("jdbc:oracle:thin:@oracle.students.itee.uq.edu.au:1521:iteeo", "CSSE3004GF", "pass123");
         } catch(Exception ex){
             System.out.println(ex.toString());
         }
         return conn;
     }
+    
 }
