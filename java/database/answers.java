e68cf6606153c5e80447929ba8656bd270e966f7
David Fairbrother <d.fairbrother@uq.edu.au>
2011-05-29 12:38:31 +1000

    * Correct connect

--- java/database/answers.java
+++ java/database/answers.java
@@ -45,7 +45,7 @@ public class answers {
             /* Load the Oracle JDBC Driver and register it. */
             DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
             /* Open a new connection */
-            conn = DriverManager.getConnection("jdbc:oracle:thin:@oracle.students.itee.uq.edu.au:1521:iteeo", "CSSE3004GF", "pass123");
+            conn = DriverManager.getConnection("jdbc:oracle:thin:@oracle.students.itee.uq.edu.au:1521:iteeo", "s4203658", "tiara9");
         } catch(Exception ex){
             System.out.println(ex.toString());
         }
