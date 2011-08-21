26bb0aaa74535aa6ae8790003efd49b027409107
unknown <louisstow@gmail.com>
2011-08-21 12:23:47 +1000

    * Changed login

--- web/model/Login.js
+++ web/model/Login.js
@@ -1,9 +1,17 @@
 $("#login-submit").click(function() {
 	var user = $("#username").val(),
-		pass = $("#password").val();
+		pass = $("#password").val(),
+		url;
+	
+	if($("#comp").val() === "A") {
+		url = "admin/webuser-login.jsp";
+	} else {
+		url = "api/admin-dologin.jsp";
+	}
 	
 	console.log("LOGIN", user, pass);
-	dbPoll.api("api/admin-dologin.jsp", {username: user, password: pass}, function(data) {
+	
+	dbPoll.api(url, {username: user, password: pass}, function(data) {
 		if(data.username) {
 			$("#top span.name").text(data.username);
 			dbPoll.go("Home");
