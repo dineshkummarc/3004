8532f0b175a8ca18011ed826956c47eb92a65ebe
unknown <louisstow@gmail.com>
2011-10-07 16:42:05 +1000

    * Left nav

--- web/model/Login.js
+++ web/model/Login.js
@@ -20,6 +20,28 @@ $("#login-submit").click(function() {
 		if(data.username) {
 			$("#top span.name").text(data.username);
 			dbPoll.role = data.role;
+			var html = "";
+			
+			switch(data.role) {
+				case 6:
+					html += "<li><a href=':ManageUsers'>Manage Users</a></li>\
+							<li><a href=':AddPollCreator'>Add Poll Creator</a></li>";
+				case 5:
+					html += "<li><a href=':ManagePolls'>Manage Polls</a></li>\
+							<li><a href=':AddPollCreator'>Add Poll Creator</a></li>";
+				case 4:
+					html += "<li><a href=':EditPoll'>Edit Poll</a></li>"
+				case 3:
+					html += "<li><a href=':Location'>Change Location</a></li>\
+							<li><a href=':Generate'>Generate Report</a></li>\
+							<li><a href=':ConductPoll'>Conduct Poll</a></li>";
+				case 2:
+				case 1:
+					html += "<li><a href=':PollIndex'>Poll Index</a></li>";
+			}
+			
+			$("#nav ul").html(html);
+			
 			dbPoll.go("Home");
 		}
 	});
