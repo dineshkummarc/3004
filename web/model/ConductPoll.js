638d4336f9817f5e26b83eab3d1b913f261eedfc
unknown <louisstow@gmail.com>
2011-10-09 10:25:54 +1000

    * FUUUK

--- web/model/ConductPoll.js
+++ web/model/ConductPoll.js
@@ -6,4 +6,24 @@ var html = "<embed codebase='http://localhost:80/'\
       userid='"+dbPoll.userID+"'\
       pluginspage='http://java.sun.com/j2se/1.5.0/download.html'/>";
 	  
-$("#container").html(html);
+$("#applet").html(html);
+
+dbPoll.api("getpollsbyuser.jsp", function(data) {
+	var i = 0, l = data.length,
+		poll, html = "";
+	
+	for(; i < l; ++i) {
+		poll = data[i];
+		html += "<option value='"+poll.id+"'>"+poll.name+"</option>";
+	}
+	
+	$("#poll").html(html);
+});
+
+$("#send").click(function() {
+	var to = $("#to").val(),
+		poll = $("#poll").val(),
+		msg = $("#msg").val();
+		
+	dbPoll.api("api/", {});
+});
