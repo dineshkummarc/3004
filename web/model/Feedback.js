e2667803793e066bc8ea02035b5ae622c5c76af2
unknown <louisstow@gmail.com>
2011-10-09 18:30:37 +1000

    * Grouped question data

--- web/model/Feedback.js
+++ web/model/Feedback.js
@@ -1,10 +1,21 @@
 dbPoll.api("api/getfeedback.jsp", {pollID: dbPoll.q.id}, function(data) {
 	var i = 0, l = data.length, poll,
-		html = "";
+		html = "",
+		feedb = {};
 	
+	//group it
 	for(; i < l; ++i) {
-		poll = data[i];
-		html += "<p>" + poll + "</p>";
+		if(!feedb[data[i].question]) feedb[data[i].question] = [];
+		feedb[data[i].question].push(data[i]);
+	}
+	
+	//grouping
+	for(var q in feedb) {
+		html += "<h2>" + q + "</h2>";
+		for(i = 0; i < feedb[q].length; ++i) {
+			poll = feedb[q][i];
+			html += "<blockquote>" + poll.text + "</blockquote>";
+		}
 	}
 	
 	$("#fb").html(html);
